dddca160a0857443d465af9c34c3a1b2a0692beb
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-28 17:01:05 +0700

    / add code  task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task13
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()); // ввод чисел  
+if(a > 100)
+    Console.Write(a);
+else
+    Console.Write("Третьей цифры нет");
+
+int a3 = a % 10;
+Console.Write(a3 % 10); 
