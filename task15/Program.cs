6971a83721c17d1b40610306712333eeab3cbee3
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-28 11:39:21 +0700

    / add code task 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 15
+Console.Clear();
+Console.Write("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+while (a < 1 || a > 7)
+{
+    Console.Write("Вы ошиблись!\aВведите день недели: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a == 6 || a == 7)
+    Console.Write("yes");
+else
+    Console.Write("no");    
