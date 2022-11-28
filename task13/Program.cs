b8536030d0bec3d09466300f3759a446be401d94
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-28 23:30:53 +0700

    / add new code  task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -2,10 +2,12 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine()); // ввод чисел  
-if(a > 100)
-    Console.Write(a);
-else
+if(a < 100)
     Console.Write("Третьей цифры нет");
+else
+{
+    while (a > 1000)
+        a = a / 10;
+}    
 
-int a3 = a % 10;
-Console.Write(a3 % 10); 
+Console.Write(a % 10); 
